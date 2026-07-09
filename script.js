304b90211588a34283056b083c1c46736f24a106
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-07-09 22:40:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-// 0. Start ->
-// a. INPUT players name, and render
-
-const player = function (playerNo) {
-  // const input = prompt(`Enter Player ${playerNo} name: `);
-  const input = false;
-  const buttonId = `#player${playerNo}`;
-
-  const player = !input ? `Player ${playerNo}` : input;
-  const mark = playerNo == 1 ? "X" : "O";
-
-  document.querySelector(buttonId).textContent = player;
-
-  return { name: player, mark };
+const ticTacToe = function () {
+  const player1 = createPlayer(1);
+  const player2 = createPlayer(2);
+  const gameBoard = createGameBoard();
+  startGame(player1, player2, gameBoard);
 };
 
-const createGameBoard = function () {
-  const store = new Array(10);
-  const displayBoxContainer = document.querySelector("section");
+const startGame = function (player1, player2, gameBoard) {
+  const store = gameBoard.store;
   let currentMark = "X";
+
+  gameBoard.displayBoxContainer.addEventListener("click", handleClickEvent);
 
   function getCurrentMark() {
     return currentMark;
@@ -55,12 +47,6 @@ const createGameBoard = function () {
       [3, 5, 7],
     ];
 
-    // a.  1,2,3 is "X" or "O"?
-    //     or, 4,5,6 is "X" or "O"?
-    //     or, 7,8,9 is "X" or "O"?
-    //     or, 1,5,9 is "X" or "O"?
-    //     or, 3,5,7 is "X" or "O"?
-
     for (const pattern of possibleMatches) {
       if (hasMatch(...pattern)) return pattern;
     }
@@ -68,22 +54,42 @@ const createGameBoard = function () {
     return false;
   }
 
-  function storeInGameBoard(id, currentMark) {
+  function storeInGameBoard(id) {
     store[id] = currentMark;
     console.log(store);
   }
 
-  function changeAndDisableBox(box, currentMark) {
+  function changeAndDisableBox(box) {
     box.textContent = currentMark;
     box.disabled = true;
   }
 
-  function addEvent(callback) {
-    displayBoxContainer.addEventListener("click", callback);
-  }
-
   function getWinningMark(match) {
     console.log(store[match[0]]);
+  }
+
+  function handleClickEvent(e) {
+    e.stopPropagation();
+    if (e.target.tagName !== "BUTTON") return;
+    const box = e.target;
+    const id = +box.dataset.id;
+    setCurrentMark();
+    changeAndDisableBox(box);
+    storeInGameBoard(id);
+    const clickedBoxCount = countClickedBox();
+
+    if (clickedBoxCount < 5) return;
+    const match = getMatch(); // return an patter array e.g. [1,2,3]
+
+    console.log(match);
+    if (!match && clickedBoxCount < 9) return; //
+
+    if (!match) {
+      console.log("IT'S A TIE.");
+      return;
+    }
+
+    getWinningMark();
   }
 
   return {
@@ -92,72 +98,25 @@ const createGameBoard = function () {
     getMatch,
     storeInGameBoard,
     getWinningMark,
-    addEvent,
     getCurrentMark,
     setCurrentMark,
   };
 };
+const createGameBoard = function () {
+  const store = new Array(10);
+  const displayBoxContainer = document.querySelector("section");
+  return { store, displayBoxContainer };
+};
+const createPlayer = function (playerNo) {
+  // const input = prompt(`Enter Player ${playerNo} name: `);
+  const input = false;
+  const buttonId = `#player${playerNo}`;
 
-const newGame = function () {
-  const player1 = player(1);
-  const player2 = player(2);
-  const gameBoard = createGameBoard();
-  gameBoard.addEvent(handleClickEvent);
+  const player = !input ? `Player ${playerNo}` : input;
+  const mark = playerNo == 1 ? "X" : "O";
 
-  function handleClickEvent(e) {
-    e.stopPropagation();
-    if (e.target.tagName !== "BUTTON") return;
-    // 1. Player1 click to a box and assign 'X' to it | Player2 click to a box and assign "O"
-    const box = e.target;
-    const id = +box.dataset.id;
-
-    // 2.
-    // a.That box is disabled and value is changed
-    // odd "X"
-    //  even "O"
-    const currentMark = gameBoard.getCurrentMark();
-    gameBoard.setCurrentMark();
-    gameBoard.changeAndDisableBox(box, currentMark);
-
-    //b. store it in the array
-    gameBoard.storeInGameBoard(id, currentMark);
-
-    // 3.
-
-    //  a. count the number of boxes that are clicked
-
-    //4. if total clicked box is less than 5, do nothigh
-    const clickedBoxCount = gameBoard.countClickedBox();
-
-    if (clickedBoxCount < 5) return;
-
-    // 5.
-
-    const match = gameBoard.getMatch(); // return an patter array e.g. [1,2,3]
-
-    console.log(match);
-
-    //     b. if neither "X" or "O":
-    //     c. if all boxes are clicked go to step 8.
-    //     d. else go to step 1.
-
-    if (!match && clickedBoxCount < 9) return; //
-
-    // 8.  It's a tie
-    // 6.  Player1 wins, go to step 9
-    // 7.  Player2 wins, go to step 9
-
-    if (!match) {
-      console.log("IT'S A TIE.");
-      return;
-    }
-
-    // return the winning mark "X" or "O"
-    // console.log(gameBoard[match[0]]);
-  }
-
-  // 9.  Show result
-  // 10. End
+  document.querySelector(buttonId).textContent = player;
+  return { name: player, mark };
 };
 
-newGame();
+document.getElementById("start").addEventListener("click", ticTacToe);
